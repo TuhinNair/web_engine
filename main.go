101d4540d92d8ddcdd66b7be288876a78b8a18fe
Tuhin Nair <tuhinnair444@gmail.com>
2020-08-19 01:03:53 +0700

    add route scaffolding and shiftPath func

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"net/http"
+	"path"
+	"strconv"
+	"strings"
 )
 
 type Route struct {
@@ -62,4 +67,44 @@ func (h *App) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 
 	next.Handler.ServeHTTP(w, r)
+}
+
+type User struct{}
+
+func (u *User) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	var head string
+	head, r.URL.Path = shiftPath(r.URL.Path)
+	if head == "profile" {
+		u.Profile(w, r)
+		return
+	} else if head == "detail" {
+		head, _ := shiftPath(r.URL.Path)
+		i, err := strconv.Atoi(head)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		ctx := r.Context()
+		ctx = context.WithValue(ctx, ctxUserId, i)
+		u.Detail(w, r.WithContext(ctx))
+		return
+	}
+
+	http.Error(w, "not found", http.StatusNotFound)
+}
+
+func (u *User) Detail(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	v := ctx.Value(ctxTestKey)
+	id := ctx.Value(ctxUserId)
+	w.Write([]byte(fmt.Sprintf("value of context us %s for user id %d", v, id)))
+}
+
+func shiftPath(p string) (head, tail string) {
+	p = path.Clean("/" + p)
+	i := strings.Index(p[1:], "/") + 1
+	if i <= 0 {
+		return p[1:], "/"
+	}
+	return p[1:i], p[1:]
 }
